a7b27d934db924a195159215dc0c97a8f7dc57e8
mNicolasPannunzio <mNicolasPannunzio@gmail.com>
2026-08-28 16:06:20 -0300

    feat(db): setup SQL scripts folder and Profile table
    - Created MobileSolutionsDB database
    - Added ScriptsSQL/01_CreateSchema.sql
    - Implemented DDL script for Profile table

---
+++ 01_CreateSchema.sql
@@ -0,0 +1,8 @@
+﻿-- 1. Tabla de Perfiles / Roles
+CREATE TABLE Profile (
+    profile_id INT IDENTITY(1,1) NOT NULL,
+    description VARCHAR(100) NOT NULL,
+    status BIT NOT NULL DEFAULT 1,
+    CONSTRAINT PK_Profile PRIMARY KEY (profile_id),
+    CONSTRAINT UQ_Profile_Description UNIQUE (description) 
+);
