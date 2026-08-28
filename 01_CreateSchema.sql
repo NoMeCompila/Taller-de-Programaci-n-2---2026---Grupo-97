3c1f768a1e6664f58124ea9861800a6611c173b8
mNicolasPannunzio <mNicolasPannunzio@gmail.com>
2026-08-28 16:46:12 -0300

    feat(db): add Customer table schema with validations

--- 01_CreateSchema.sql
+++ 01_CreateSchema.sql
@@ -24,3 +24,27 @@ CREATE TABLE Brand (
     CONSTRAINT PK_Brand PRIMARY KEY (brand_id),
     CONSTRAINT UQ_Brand_Name UNIQUE (name) 
 );
+
+-- 4. Tabla de Clientes
+CREATE TABLE Customer (
+    customer_id INT IDENTITY(1,1) NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    lastname VARCHAR(100) NOT NULL,
+    dni VARCHAR(8) NOT NULL,
+    sex VARCHAR(10) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    phone VARCHAR(15) NULL,
+    address VARCHAR(100) NULL,
+    birth DATE NOT NULL,
+    nationality VARCHAR(100) NOT NULL,
+    locality VARCHAR(100) NOT NULL,
+    register_date DATETIME2(7) NOT NULL DEFAULT GETDATE(),
+    status BIT NOT NULL DEFAULT 1,
+    CONSTRAINT PK_Customer PRIMARY KEY (customer_id),
+    CONSTRAINT UQ_Customer_DNI UNIQUE (dni), 
+    CONSTRAINT UQ_Customer_Email UNIQUE (email), 
+    CONSTRAINT CHK_Customer_Sex CHECK (sex IN ('Masculino', 'Femenino', 'Otro')), 
+    CONSTRAINT CHK_Customer_Birth CHECK (birth < CAST(GETDATE() AS DATE)), 
+    CONSTRAINT CHK_Customer_Email CHECK (email LIKE '%_@_%.%') 
+);
+
