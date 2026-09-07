4216229a529bf23227d68c87ced72a18ec0a7fad
mNicolasPannunzio <mNicolasPannunzio@gmail.com>
2026-09-07 13:58:13 -0300

    Feat(UI): Created SaleView (first instance) without validations and functionalities. Only visual components

--- MobileSolutions.UILayer/MainForm.Designer.cs
+++ MobileSolutions.UILayer/MainForm.Designer.cs
@@ -45,6 +45,7 @@ namespace MobileSolutions.UILayer
             tabPage5 = new TabPage();
             materialLabel5 = new MaterialSkin.Controls.MaterialLabel();
             tabPage6 = new TabPage();
+            saleView1 = new SaleView();
             materialLabel6 = new MaterialSkin.Controls.MaterialLabel();
             tabPage7 = new TabPage();
             materialLabel7 = new MaterialSkin.Controls.MaterialLabel();
@@ -80,7 +81,7 @@ namespace MobileSolutions.UILayer
             materialTabControl1.Multiline = true;
             materialTabControl1.Name = "materialTabControl1";
             materialTabControl1.SelectedIndex = 0;
-            materialTabControl1.Size = new Size(1800, 1035);
+            materialTabControl1.Size = new Size(1872, 1035);
             materialTabControl1.TabIndex = 0;
             // 
             // tabPage1
@@ -90,7 +91,7 @@ namespace MobileSolutions.UILayer
             tabPage1.Margin = new Padding(3, 4, 3, 4);
             tabPage1.Name = "tabPage1";
             tabPage1.Padding = new Padding(3);
-            tabPage1.Size = new Size(1792, 1002);
+            tabPage1.Size = new Size(1864, 1002);
             tabPage1.TabIndex = 0;
             tabPage1.Text = "Home";
             tabPage1.UseVisualStyleBackColor = true;
@@ -101,7 +102,7 @@ namespace MobileSolutions.UILayer
             homeView1.Location = new Point(3, 3);
             homeView1.Margin = new Padding(3, 5, 3, 5);
             homeView1.Name = "homeView1";
-            homeView1.Size = new Size(1786, 996);
+            homeView1.Size = new Size(1858, 996);
             homeView1.TabIndex = 0;
             // 
             // tabPage2
@@ -112,7 +113,7 @@ namespace MobileSolutions.UILayer
             tabPage2.Margin = new Padding(3, 4, 3, 4);
             tabPage2.Name = "tabPage2";
             tabPage2.Padding = new Padding(3);
-            tabPage2.Size = new Size(1774, 1002);
+            tabPage2.Size = new Size(1846, 1002);
             tabPage2.TabIndex = 1;
             tabPage2.Text = "Usuarios";
             tabPage2.UseVisualStyleBackColor = true;
@@ -122,7 +123,7 @@ namespace MobileSolutions.UILayer
             userView1.Dock = DockStyle.Fill;
             userView1.Location = new Point(3, 3);
             userView1.Name = "userView1";
-            userView1.Size = new Size(1768, 996);
+            userView1.Size = new Size(1840, 996);
             userView1.TabIndex = 3;
             // 
             // materialLabel3
@@ -144,7 +145,7 @@ namespace MobileSolutions.UILayer
             tabPage3.Location = new Point(4, 29);
             tabPage3.Margin = new Padding(3, 4, 3, 4);
             tabPage3.Name = "tabPage3";
-            tabPage3.Size = new Size(1774, 1002);
+            tabPage3.Size = new Size(1846, 1002);
             tabPage3.TabIndex = 2;
             tabPage3.Text = "Clientes";
             tabPage3.UseVisualStyleBackColor = true;
@@ -154,7 +155,7 @@ namespace MobileSolutions.UILayer
             customerView1.Dock = DockStyle.Fill;
             customerView1.Location = new Point(0, 0);
             customerView1.Name = "customerView1";
-            customerView1.Size = new Size(1774, 1002);
+            customerView1.Size = new Size(1846, 1002);
             customerView1.TabIndex = 4;
             // 
             // materialLabel2
@@ -176,7 +177,7 @@ namespace MobileSolutions.UILayer
             tabPage4.Location = new Point(4, 29);
             tabPage4.Margin = new Padding(3, 4, 3, 4);
             tabPage4.Name = "tabPage4";
-            tabPage4.Size = new Size(1792, 1002);
+            tabPage4.Size = new Size(1846, 1002);
             tabPage4.TabIndex = 3;
             tabPage4.Text = "Productos";
             tabPage4.UseVisualStyleBackColor = true;
@@ -186,7 +187,7 @@ namespace MobileSolutions.UILayer
             productView1.Dock = DockStyle.Fill;
             productView1.Location = new Point(0, 0);
             productView1.Name = "productView1";
-            productView1.Size = new Size(1792, 1002);
+            productView1.Size = new Size(1846, 1002);
             productView1.TabIndex = 4;
             // 
             // materialLabel4
@@ -207,7 +208,7 @@ namespace MobileSolutions.UILayer
             tabPage5.Location = new Point(4, 29);
             tabPage5.Margin = new Padding(3, 4, 3, 4);
             tabPage5.Name = "tabPage5";
-            tabPage5.Size = new Size(1774, 1002);
+            tabPage5.Size = new Size(1846, 1002);
             tabPage5.TabIndex = 4;
             tabPage5.Text = "Marcas";
             tabPage5.UseVisualStyleBackColor = true;
@@ -226,14 +227,23 @@ namespace MobileSolutions.UILayer
             // 
             // tabPage6
             // 
+            tabPage6.Controls.Add(saleView1);
             tabPage6.Controls.Add(materialLabel6);
             tabPage6.Location = new Point(4, 29);
             tabPage6.Margin = new Padding(3, 4, 3, 4);
             tabPage6.Name = "tabPage6";
-            tabPage6.Size = new Size(1774, 1002);
+            tabPage6.Size = new Size(1864, 1002);
             tabPage6.TabIndex = 5;
             tabPage6.Text = "Venta";
             tabPage6.UseVisualStyleBackColor = true;
+            // 
+            // saleView1
+            // 
+            saleView1.Dock = DockStyle.Fill;
+            saleView1.Location = new Point(0, 0);
+            saleView1.Name = "saleView1";
+            saleView1.Size = new Size(1864, 1002);
+            saleView1.TabIndex = 4;
             // 
             // materialLabel6
             // 
@@ -253,7 +263,7 @@ namespace MobileSolutions.UILayer
             tabPage7.Location = new Point(4, 29);
             tabPage7.Margin = new Padding(3, 4, 3, 4);
             tabPage7.Name = "tabPage7";
-            tabPage7.Size = new Size(1774, 1002);
+            tabPage7.Size = new Size(1846, 1002);
             tabPage7.TabIndex = 6;
             tabPage7.Text = "Historial De Ventas";
             tabPage7.UseVisualStyleBackColor = true;
@@ -276,7 +286,7 @@ namespace MobileSolutions.UILayer
             tabPage8.Location = new Point(4, 29);
             tabPage8.Margin = new Padding(3, 4, 3, 4);
             tabPage8.Name = "tabPage8";
-            tabPage8.Size = new Size(1774, 1002);
+            tabPage8.Size = new Size(1846, 1002);
             tabPage8.TabIndex = 7;
             tabPage8.Text = "Reportes";
             tabPage8.UseVisualStyleBackColor = true;
@@ -303,7 +313,7 @@ namespace MobileSolutions.UILayer
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1806, 1102);
+            ClientSize = new Size(1878, 1102);
             Controls.Add(materialTabControl1);
             DrawerTabControl = materialTabControl1;
             FormBorderStyle = FormBorderStyle.FixedSingle;
@@ -357,6 +367,7 @@ namespace MobileSolutions.UILayer
         private CustomerView customerView1;
         private HomeView homeView1;
         private ProductView productView1;
+        private SaleView saleView1;
     }
 }
 
---
+++ MobileSolutions.UILayer/SaleView.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Text;
+using System.Windows.Forms;
+using FontAwesome.Sharp;
+using MaterialSkin.Controls;
+
+namespace MobileSolutions.UILayer
+{
+    public partial class SaleView : UserControl
+    {
+        public SaleView()
+        {
+            InitializeComponent();
+
+            // Asignar íconos FontAwesome a los botones de acción
+            btnAddDetail.Icon = IconChar.CartPlus.ToBitmap(Color.White);
+            btnTotalSale.Icon = IconChar.Coins.ToBitmap(Color.White);
+            btnConfirmSale.Icon = IconChar.CircleCheck.ToBitmap(Color.White);
+            btnCancelSale.Icon = IconChar.Trash.ToBitmap(Color.White);
+
+            // Íconos de los botones pequeños junto a cada campo
+            btnCustomer.Icon = IconChar.UserTag.ToBitmap(Color.White);
+            btnSaleNumber.Icon = IconChar.Barcode.ToBitmap(Color.White);
+            btnPaymentType.Icon = IconChar.MoneyCheckDollar.ToBitmap(Color.White);
+            btnQuantity.Icon = IconChar.LayerGroup.ToBitmap(Color.White);
+            btnUnitaryPrice.Icon = IconChar.DollarSign.ToBitmap(Color.White);
+            btnProductCode.Icon = IconChar.Barcode.ToBitmap(Color.White);
+
+            // Ícono del título del panel
+            picSaleTitle.IconChar = IconChar.Shopify;
+            picSaleTitle.IconColor = Color.White;
+        }
+
+        
+    }
+
+}
