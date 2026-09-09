ca3fa000e29c9c5962469a6bfe2b6999152d3b13
mNicolasPannunzio <mNicolasPannunzio@gmail.com>
2026-09-09 19:16:24 -0300

    Feat(UI): Created UserControl 'ReportsView', already linked to his TabPage in MainForm

---
+++ MobileSolutions.UILayer/ReportsView.cs
@@ -0,0 +1,27 @@
+﻿using FontAwesome.Sharp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TreeView;
+
+namespace MobileSolutions.UILayer
+{
+    public partial class ReportsView : UserControl
+    {
+        public ReportsView()
+        {
+            InitializeComponent();
+
+            // Ícono del título del panel
+            picReportsTitle.IconChar = IconChar.ChartColumn;
+            picReportsTitle.IconColor = Color.White;
+
+            picProximamente.IconChar = IconChar.ScrewdriverWrench;
+            picProximamente.IconColor = Color.Yellow;
+        }
+    }
+}
