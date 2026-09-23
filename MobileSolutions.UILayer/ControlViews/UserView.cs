5bb2ab0d9ac38f7e3f4ba7100be0c237df06bf5f
mNicolasPannunzio <mNicolasPannunzio@gmail.com>
2026-09-23 15:43:57 -0300

    Ready For First Presentation of the Proyect

--- MobileSolutions.UILayer/ControlViews/UserView.cs
+++ MobileSolutions.UILayer/ControlViews/UserView.cs
@@ -240,16 +240,7 @@ namespace MobileSolutions.UILayer
 
         private bool ValidateFields(bool isUpdate = false)
         {
-            if (dtpBirth.Value.Date > DateTime.Today.AddYears(-18))
-            {
-                MaterialMessageBox.Show(
-                    "El usuario debe ser mayor de edad (18 años o más).",
-                    "Validación",
-                    MessageBoxButtons.OK,
-                    MessageBoxIcon.Warning);
-                dtpBirth.Focus();
-                return false;
-            }
+            
 
             if (string.IsNullOrWhiteSpace(txtName.Text)
                 || string.IsNullOrWhiteSpace(txtLastname.Text)
@@ -323,6 +314,17 @@ namespace MobileSolutions.UILayer
             {
                 MaterialMessageBox.Show(
                     "La fecha de nacimiento debe ser anterior a la fecha actual.",
+                    "Validación",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Warning);
+                dtpBirth.Focus();
+                return false;
+            }
+
+            if (dtpBirth.Value.Date > DateTime.Today.AddYears(-18))
+            {
+                MaterialMessageBox.Show(
+                    "El usuario debe ser mayor de edad (18 años o más).",
                     "Validación",
                     MessageBoxButtons.OK,
                     MessageBoxIcon.Warning);
